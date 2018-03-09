e7189a24d8e214e8b96a30b503b4743ff661b4cc
BeMg <gccbg04538@gmail.com>
2018-03-09 11:04:33 +0800

    can render search result

--- hw1/app.py
+++ hw1/app.py
@@ -1,4 +1,5 @@
 from flask import render_template, Flask, request, jsonify
+from main import query
 
 app = Flask(__name__)
 
@@ -9,8 +10,8 @@ def mainpage():
 @app.route('/search')
 def search_result():
     keyword = request.args.get('keyword', ' ', type=str)
-    print(keyword)
-    return jsonify(result=1)
+    res = query(keyword)
+    return jsonify(res=res)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
---
+++ hw1/news_data/rec2json.py
@@ -0,0 +1,36 @@
+import os
+import json
+import glob
+
+filename = glob.glob("ettoday*.rec")
+
+data = []
+for fn in filename:
+    cnt = 0
+    with open(fn, "r") as f:
+        while True:
+            cnt += 1
+            pos = f.tell()
+            useless = f.readline()
+            url = f.readline()
+            url = url[3:]
+            url = url.strip('\n')
+            title = f.readline()
+            title = title[3:]
+            title = title.strip('\n')
+            useless = f.readline()
+            maintext = f.readline()
+            maintext = maintext.strip()
+            after_pos = f.tell()
+            print("{}: {}".format(fn, cnt))
+            # print("{}:{}:{}".format(url, title, maintext))
+            if pos == after_pos:
+                break
+            else:
+                data.append({
+                    'url':url, 
+                    'title':title, 
+                    'body':maintext})
+
+
+
